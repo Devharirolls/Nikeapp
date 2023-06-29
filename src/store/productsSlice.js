50b083f7c88847f7d2cec5ad580c230fdf7a74a5
Devharirolls <138101854+Devharirolls@users.noreply.github.com>
2023-06-29 13:39:58 +0530

    Update productsSlice.js

--- src/store/productsSlice.js
+++ src/store/productsSlice.js
@@ -19,21 +19,3 @@ export const productsSlice = createSlice({
 
 
 
-// import { createSlice } from "@reduxjs/toolkit";
-// import products from '../data/products';
-
-// const initialState = {
-//     products:products,
-//     selectedProduct:null
-// };
-
-// export const productsSlice = createSlice({
-//     name:"products",
-//     initialState:initialState,
-//     reducers: {
-//         setSelectedProduct: (state,action)=>{
-//             const productId = action.payload;
-//             state.selectedProduct = state.products.find((p)=>p.id === productId);
-//         },
-//     },
-// });
